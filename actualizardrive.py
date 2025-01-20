48414d750588bbc0c6322d875a2a41016789c6c3
rosamalave <rosamalavework@gmail.com>
2025-01-20 11:00:39 -0400

    #

--- actualizardrive.py
+++ actualizardrive.py
@@ -1,3 +1,5 @@
+import os
+import json
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from google.oauth2.service_account import Credentials
@@ -6,13 +8,21 @@ from google.oauth2.service_account import Credentials
 class conexiondrive():
     def __init__(self, idfiledrive, nombrearchivodrive, rutaarchivolocal):
         SCOPES = ['https://www.googleapis.com/auth/drive']
-        creds = Credentials.from_service_account_file('R:/Respaldo/singular-hash-437514-p9-9bc09e62582b.json', scopes=SCOPES)
+
+        # Verificar si el script se está ejecutando en un GitHub Action
+        if os.environ.get("GITHUB_ACTIONS") == "true":
+            # Cargar las credenciales desde la variable de entorno
+            google_credentials_json = os.environ.get("GOOGLE_CREDENTIALS")
+            creds = Credentials.from_service_account_info(json.loads(google_credentials_json), scopes=SCOPES)
+        else:
+            # Ruta local a las credenciales
+            rutacred = 'R:/Respaldo/singular-hash-437514-p9-9bc09e62582b.json'
+            creds = Credentials.from_service_account_file(rutacred, scopes=SCOPES)
+
         self.drive_service = build('drive', 'v3', credentials=creds)
-        self.nombrearchivodrive=nombrearchivodrive
-        # ID del archivo de Google Drive (se obtiene de la URL del archivo en Drive)
-        self.file_id = idfiledrive 
-        # Ruta al archivo local que quieres subir
-        self.local_file_path = rutaarchivolocal #'R:/Respaldo/Rosa/TRABAJO/transfihermanos/automatizacion/automatizacion-binance-transfi/tasas-transfi.xlsx'
+        self.nombrearchivodrive = nombrearchivodrive
+        self.file_id = idfiledrive  # ID del archivo de Google Drive (se obtiene de la URL del archivo en Drive)
+        self.local_file_path = rutaarchivolocal  # Ruta al archivo local que quieres subir
 
     def sincronizararchivo(self):
         # Subir y reemplazar el archivo existente
